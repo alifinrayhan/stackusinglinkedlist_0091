73ef0784b1fd895d2051c6847b65a29f42bf1658
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-10 13:59:06 +0700

    int push dan void pop

--- Stack linked List.cpp
+++ Stack linked List.cpp
@@ -26,5 +26,23 @@ public:
     } 
 
     // Push operetion: Insert an element onto the top of the stack
-           
+    int push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        cout << "Push value: " << value << endl;
+        return value;
+    } 
+
+    // Pop operation: Remove the topmost element from the stack
+    void pop(){
+        if (isEmpty()) {
+            cout << "Stack is empty. " << endl;
+        }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; // Update the top pointer to the next node
+
+    }
+
+    //      
 }
