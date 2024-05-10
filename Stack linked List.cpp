6f212aaebc4b63c13dcb6f81f1380542941d8494
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-10 13:47:58 +0700

    Class Node

--- Stack linked List.cpp
+++ Stack linked List.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+
+//NOde class reprenting a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }    
+};
